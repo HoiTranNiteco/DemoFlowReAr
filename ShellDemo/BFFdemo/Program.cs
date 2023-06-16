d6fa7a1fe76b962d94ccac01e2182043be1c91d9
Hoi_Tran_Niteco <hoi.tran@niteco.se>
2023-06-16 16:35:22 +0700

    Add

---
+++ ShellDemo/BFFdemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+	.ConfigureFunctionsWorkerDefaults(builder =>
+	{
+		//builder
+		//	.AddApplicationInsights(options =>
+		//	{
+		//		// Configure the underlying ApplicationInsightsServiceOptions
+		//		// This property configuration serves solely for demonstration purposes, and it is not mandatory to replicate the same property and its associated value in your own application.
+		//		options.ApplicationVersion = "v1";
+		//	})
+		//	.AddApplicationInsightsLogger();
+	})
+	.ConfigureServices(s =>
+	{
+		//s.AddSingleton<IHttpResponderService, DefaultHttpResponderService>();
+		//s.Configure<LoggerFilterOptions>(options =>
+		//{
+		//	// The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+		//	// Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+		//	LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+		//		== "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+		//	if (toRemove is not null)
+		//	{
+		//		options.Rules.Remove(toRemove);
+		//	}
+		//});
+	})
+	.Build();
